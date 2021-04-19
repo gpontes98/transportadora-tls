dc0673cd2df3d1f567f8512cfe5a46cd8d623d16
Gabriel de Barros Pontes <gabrielpontes98@gmail.com>
2021-04-18 21:48:33 -0300

    Resumindo toda a função do sendmailer no app.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
-import sendMail from './scripts/sendMail'; 
+const nodemailer = require('nodemailer');
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
@@ -13,14 +12,32 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post("/sendmail", async function(req,res){
-    const nome = req.body.nome;
-    const email = req.body.email;
-    const telefone = req.body.telefone;
-    const mensagem = req.body.mensagem;
-
-    sendMail(nome, email, telefone, mensagem);
-    res.send('Formulário recebido');
+app.post("/sendmail", async function(req,res){    
+    let transporter = nodemailer.createTransport({ 
+        service: 'gmail', 
+        auth: { 
+            user: 'transporteelogisticasilva@gmail.com', 
+            pass: 'tlsilva21' 
+            } 
+    });
+  
+    const mailOptions = {
+        from: 'gabrielpontes98@gmail.com', // sender address
+        to: 'gabrielpontes98@gmail.com', // receiver (use array of string for a list)
+        subject: 'Contato TLS', // Subject line
+        html: `
+        <p>Nome: ${req.body.nome}</p>
+        <p>Email: ${req.body.email}</p>
+        <p>Telefone: ${req.body.telefone}</p>
+        <p>Mensagem: ${req.body.mensagem}</p>`// plain text body
+    };
+  
+    transporter.sendMail(mailOptions, (err, info) => {
+        if(err)
+            res.sendFile(__dirname + "/public/erro.html");
+        else
+          res.sendFile(__dirname + "/public/sucesso.html");
+    });
 })
 
 app.listen(3001);
