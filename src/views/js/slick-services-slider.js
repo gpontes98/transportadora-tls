a8cc1011e24d39847f9b662120c4ebfd2011010e
Gabriel de Barros Pontes <gabrielpontes98@gmail.com>
2021-05-03 20:22:29 -0300

    Adicionando depoimentos

--- src/views/js/slick-services-slider.js
+++ src/views/js/slick-services-slider.js
@@ -1,29 +1,10 @@
 $(document).ready(function(){
-    $('.services').slick({
+    $('.testimonials').slick({
         dots: true,
         arrows:false,
         infinite: true,
         speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [
-            {
-                breakpoint: 990,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                    }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                    }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-        ]
+        slidesToShow: 1,
+        slidesToScroll: 1
     });
 });
